a0575ae2f04c9daa08c7bd2d666788bcfd8b159d
Ayush <ayush.kanduri@gmail.com>
2022-08-03 19:34:06 +0530

    make it better

--- index.js
+++ index.js
@@ -8,61 +8,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// async function chat(data) {
-// 	try {
-// 		let info = {};
-// 		let { friend_email, chat_room, timestamp, message, user_email } = data;
-// 		let alignment = "";
-
-// 		const user = await User.findOne({ email: user_email }).populate();
-// 		const friend = await User.findOne({ email: friend_email }).populate();
-
-// 		let chat = await Chat.create({
-// 			content: message,
-// 			sender: user._id,
-// 			receiver: friend._id,
-// 			time: timestamp,
-// 			room: chat_room,
-// 		});
-
-// 		let room = await Room.findOne({
-// 			sender: user._id,
-// 			receiver: friend._id,
-// 		});
-
-// 		if (!room) {
-// 			room = await Room.findOne({
-// 				sender: friend._id,
-// 				receiver: user._id,
-// 			});
-// 		}
-
-// 		chat.save();
-// 		room.chats.push(chat);
-// 		room.save();
-// 		user.chats.push(chat);
-// 		friend.chats.push(chat);
-// 		user.save();
-// 		friend.save();
-
-// 		let sender = await User.findById(chat.sender).populate();
-// 		sender = sender.email;
-// 		info = {
-// 			message: message,
-// 			user_name: user.name,
-// 			user_email: user.email,
-// 			friend_name: friend.name,
-// 			friend_email: friend.email,
-// 			alignment: alignment,
-// 			sender: sender,
-// 			timestamp: timestamp,
-// 			chat_room: chat_room,
-// 		};
-// 		return info;
-// 	} catch (err) {
-// 		console.log("Error: ", err);
-// 	}
-// }
+async function fetchData(info) {
+	try {
+		let url = "https://the-social-book.herokuapp.com/messages/chatting";
+		let response = await fetch(url, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(info),
+		});
+		let data = await response.json();
+		return data.data.info;
+	} catch (err) {
+		console.log("Error: ", err);
+	}
+}
 
 const chatSockets = (chatServer) => {
 	const io = require("socket.io")(chatServer, { cors: { origin: "*" } });
@@ -88,7 +47,11 @@ const chatSockets = (chatServer) => {
 			io.in(data.chat_room).emit("user_left", data);
 		});
 		socket.on("send_message", async function (data) {
-			const info = await chat(data);
+			try {
+				const info = await fetchData(data);
+			} catch (err) {
+				console.log("Error: ", err);
+			}
 			io.in(data.chat_room).emit("receive_message", info);
 		});
 		socket.on("new_message", function (data) {
