009fde0565a76f053703d617391c061a395e3989
Ayush <ayush.kanduri@gmail.com>
2022-08-03 20:41:10 +0530

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
 const chatServer = require("http").Server(app);
